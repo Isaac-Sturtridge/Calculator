a7f5a97802382840a84621408029661a3df27247
izzy <eyes330@yahoo.co.uk>
2022-03-09 15:01:55 +0800

    numbers can be entered and cleared

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const display = document.getElementById('display')
 const numbers = document.querySelectorAll('.number')
-const operator = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.operator')
 const clearBtn = document.querySelector('.clear')
+let enteringNumbers = false;
 
-numbers.addEventListener('click', )
+numbers.forEach((num) => {
+    num.addEventListener('click', function(num) {
+        if(!enteringNumbers) {
+            display.innerHTML = `${num.target.textContent}`
+            enteringNumbers = true;
+        } else {
+            display.innerHTML = display.innerHTML + `${num.target.textContent}`
+        }
+        if(display.innerHTML.length > 18) {
+            display.innerHTML = "Too long!"
+            enteringNumbers = false;
+        }
+    })
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', function(operator) {
+        let num1 = parseInt(display.innerHTML);
+    })
+})
+
+clearBtn.addEventListener('click', () => {
+    display.innerHTML = '0';
+    enteringNumbers = false;
+})
 
 function add(num1, num2) {
     return num1 + num2;
