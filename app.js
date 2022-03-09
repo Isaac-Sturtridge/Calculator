1bc412d0639a169ba0fba938a925190db47776aa
izzy <eyes330@yahoo.co.uk>
2022-03-09 14:36:04 +0800

    basic html/ css and functions defined, no functionality yet

---
+++ app.js
@@ -0,0 +1,26 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2)
+}
+
+console.log(add(1,1))
+console.log(subtract(1,1))
+console.log(multiply(2,3))
+console.log(divide(3,2))
+
+console.log(operate(divide, 100, 5))
