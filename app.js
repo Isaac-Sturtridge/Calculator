dfca808ba0ea7d37f7385326688f973918e9c784
izzy <eyes330@yahoo.co.uk>
2022-03-12 22:21:15 +0800

    basic calculator working

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const display = document.getElementById('display')
 const numbers = document.querySelectorAll('.number')
 const operators = document.querySelectorAll('.operator')
 const clearBtn = document.querySelector('.clear')
+const equalBtn = document.querySelector('.equals')
 let enteringNumbers = false;
+let opOn = false;
+
+// a running total of numbers in the current calculation
+let numbersInCalc = [];
+let operatorsInCalc = [];
 
 numbers.forEach((num) => {
     num.addEventListener('click', function(num) {
+        opOn = false;
         if(!enteringNumbers) {
             display.innerHTML = `${num.target.textContent}`
             enteringNumbers = true;
@@ -13,25 +20,57 @@ numbers.forEach((num) => {
             display.innerHTML = display.innerHTML + `${num.target.textContent}`
         }
         if(display.innerHTML.length > 18) {
-            display.innerHTML = "Too long!"
-            enteringNumbers = false;
+            display.innerHTML = 'Too long!';
+            enteringNumbers = false; opOn = false;
         }
     })
 })
 
 operators.forEach((operator) => {
     operator.addEventListener('click', function(operator) {
-        let num1 = parseInt(display.innerHTML);
+        if(!opOn && display.innerHTML.length < 18) {
+            numbersInCalc.push(parseInt(display.innerHTML))
+            operatorsInCalc.push(operator.target.textContent);
+            enteringNumbers = false;
+            opOn = true;
+        } 
     })
 })
 
 clearBtn.addEventListener('click', () => {
     display.innerHTML = '0';
     enteringNumbers = false;
+    opOn = false;
+    numbersInCalc = [];
+})
+
+equalBtn.addEventListener('click', () => {
+    numbersInCalc.push(parseInt(display.innerHTML))
+    switch(operatorsInCalc[0]) {
+        case '+':
+            display.innerHTML = operate(add, numbersInCalc[0], numbersInCalc[1]);
+        break;
+        case '-':
+            display.innerHTML = operate(subtract, numbersInCalc[0], numbersInCalc[1]);
+        break;
+        case '*':
+            display.innerHTML = operate(multiply, numbersInCalc[0], numbersInCalc[1]);
+        break;
+        case '/':
+            if(numbersInCalc[1] === 0) {
+                display.innerHTML = 'Do not divide by zero';
+                break;
+            }
+            display.innerHTML = operate(divide, numbersInCalc[0], numbersInCalc[1])
+        default:
+        break;
+    }
+    enteringNumbers = false;
+    numbersInCalc = [], operatorsInCalc = [];
 })
 
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2) {
